7b4d7b44ab4a797377f6a0209f540a1e98632caf
iris112 <nglayi99@gmail.com>
2022-03-22 19:36:18 -0500

    defender sentinel autotask condition

--- sentinel_condition.ts
+++ sentinel_condition.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from "ethers";
+import fetch from "node-fetch";
 
 const theGraphURL = 'https://api.thegraph.com/subgraphs/name/sturdyfi/sturdy-fantom';
 
@@ -12,7 +13,7 @@ const checkLargestDepositCondition = async (assetAddress:string, withdrawBalance
           orderBy: currentATokenBalance
           first: 1
           orderDirection: desc
-          where: {reserve_contains: ${assetAddress}}
+          where: {reserve_contains: "${assetAddress.toLowerCase()}"}
         ) {
           currentATokenBalance
         }
@@ -20,7 +21,7 @@ const checkLargestDepositCondition = async (assetAddress:string, withdrawBalance
     }),
   })
   const result = await data.json();
-  console.log(result);
+  console.log(result.data.userReserves);
   if (result.data.userReserves.length && withdrawBalance.gt(result.data.userReserves[0].currentATokenBalance))
     return true;
   
@@ -33,8 +34,9 @@ export async function handler(payload) {
   const matches: Array<any> = [];
   const evt = conditionRequest.events[0];
 
+  console.log(evt.matchReasons);
   // On withdraw case, check the largest deposit amount > withdraw amount
-  const withdrawReasons = evt.matchReasons.filter((item) => item.signature.indexOf('Withdraw'));
+  const withdrawReasons = evt.matchReasons.filter((item) => item.signature.indexOf('Withdraw') >= 0);
   for(const reason of withdrawReasons) {
     if (await checkLargestDepositCondition(reason.params.reserve, BigNumber.from(reason.params.amount))) {
       matches.push({
@@ -55,3 +57,23 @@ export async function handler(payload) {
   
   return { matches };
 }
+// handler({ request: { body: { events: [{ matchReasons: [
+//   {
+//     type: 'event',
+//     signature: 'Withdraw(address,address,address,uint256)',
+//     args: [
+//       '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75',
+//       '0x1c5704c56855E196E6d125f2d96091DB85b3fBEE',
+//       '0x1c5704c56855E196E6d125f2d96091DB85b3fBEE',
+//       '18000000'
+//     ],
+//     params: {
+//       reserve: '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75',
+//       user: '0x1c5704c56855E196E6d125f2d96091DB85b3fBEE',
+//       to: '0x1c5704c56855E196E6d125f2d96091DB85b3fBEE',
+//       amount: '18000000'
+//     }
+//   }
+// ],
+// hash: "0x00000000000000000000000"}]}}
+// })
